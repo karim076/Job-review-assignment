2024dade52bbb83a332c375d2bc86b60e2d4a4ec
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-16 16:27:26 +0200

    Added basic html constructure

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require_once 'head.php' ?>
+	<title>Modellen</title>
+</head>
+<body>
+	<header>
+		<?php require_once 'header.php' ?>
+	</header>
+	<main>
+		
+	</main>
+	<footer>
+		<?php require_once 'footer.php' ?>
+	</footer>
+</body>
+</html>
