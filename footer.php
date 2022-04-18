8a29733ffb20a019d563d095b9aa9d7b4ed80a4b
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-18 20:05:10 +0200

    Added footer fully responsive

---
+++ footer.php
@@ -0,0 +1,30 @@
+<div class="d-flex justify-content-center whole">
+    <div class="container-xxl d-grid gap breakpoint d-flex flex-row">
+        <div class="col-md-6 xl centering">
+            <div class="none">
+                <img src="img/main/button-meerinfo.png" alt="Meer informatie button">
+                <h1 class="deg">Hoogbegaafde topmodellen</h1>
+            </div>
+            <div class="d-flex flex-row justify-content-between gapping centering">
+                <h3>(012) 3456 789</h3>
+                <h3>vragen@website.nl</h3>
+            </div>
+        </div>
+        <div class="col-md-2 xl margintop centering">
+            <div class="margintop">
+                <p>straatnaam 12</p>
+                <p>1234 AB Breda</p>
+            </div>
+            <div class="margins">
+                <p>KvK Nummer</p>
+            </div>
+        </div>
+        <div class="col-md-2 xl centering">
+            <div class="img">
+                <img src="img/icon/facebook.png" class="right imgs" alt="">
+                <img src="img/icon/linkedin.png" class="right imgs" alt="">
+                <img src="img/icon/twitter.png" class="imgs" alt="">
+            </div>
+        </div>
+    </div>
+</div>
